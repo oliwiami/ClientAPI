7af29167bc1d462940c137b38a02f2d634329e06
Oliwia Mi <o.mierkulow@gmail.com>
2022-09-21 21:14:53 +0200

    Add project files.

---
+++ ClientAPI/Models/AddRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientAPI.Models
+{
+    public class AddRequest
+    {
+        public string Pesel { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ClientAPI/Models/ClientModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ClientAPI.Models
+{
+    public class ClientModel
+    {
+        public Guid Id { get; set; }
+        public string Pesel { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
